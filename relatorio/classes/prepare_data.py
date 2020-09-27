d6f3955783fdf8385dfbfbd47cf897cacdc3b74f
EricGPTI <ericgpti@gmail.com>
2020-09-26 22:17:11 -0300

    Ajustada classe para processamento de arquivo em chunks.

--- relatorio/classes/prepare_data.py
+++ relatorio/classes/prepare_data.py
@@ -6,16 +6,12 @@ class SaveData:
     Classe para salvar dados processados.
     """
 
-    def __init__(self, file, data_exams=None):
+    def __init__(self, file):
         self.file = file
         self.reader = Reader(self.file)
-        self.create = CreateDataExams(data_exams)
 
     def process_exams(self):
         self.reader.process_exams
 
     def process_appointment(self):
         self.reader.process_appointment
-
-    def create_data(self):
-        self.create.create_exams
